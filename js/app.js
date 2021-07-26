b5176a274e042be816a678d8fb5941edf4b459e2
Wesam-Alqawasmeh <alqawasmehwesam@gmail.com>
2021-07-26 15:36:04 +0300

    create lab 02 web page following instructions

---
+++ js/app.js
@@ -0,0 +1,120 @@
+'use strict';
+
+let userName = prompt ('What is your name ?');
+let readyMassage = prompt (userName + ', are you ready for a guessing game ?').toLowerCase();
+switch(readyMassage) {
+    case 'yes' :
+        alert('lets go');
+        // console.log('yes');
+        break;
+
+        case  'y' :
+        alert('lets go');
+        // console.log('yes');
+        break;
+        
+        case 'no' :
+            alert('#-#');
+            // console.log('no');
+            break;
+
+        case  'n' :
+        alert('#-#');
+        // console.log('no');
+        break;
+}
+
+let q1 = prompt ('DO you know my name ?').toLowerCase();
+switch(q1) {
+    case 'yes' :
+        alert('^_^');
+        // console.log('yes');
+        break;
+
+        case  'y' :
+        alert('^_^');
+        // console.log('yes');
+        break;
+        
+        case 'no' :
+            alert('I am Wesam Alqawasmeh');
+            // console.log('no');
+            break;
+
+        case  'n' :
+        alert('I am Wesam Alqawasmeh');
+        // console.log('no');
+        break;
+}
+
+let q2 = prompt ('Do you know my age ?').toLowerCase();
+switch(q2) {
+    case 'yes' :
+        alert('^_^');
+        // console.log('yes');
+        break;
+
+        case  'y' :
+        alert('^_^');
+        // console.log('yes');
+        break;
+        
+        case 'no' :
+            alert('I am 23 years old');
+            // console.log('no');
+            break;
+
+        case  'n' :
+        alert('I am 23 years old');
+        // console.log('no');
+        break;
+}
+
+let q3 = prompt ('DO i like SHAWERMA ?').toLowerCase();
+switch(q3) {
+    case 'yes' :
+        alert('Yeah sure');
+        // console.log('yes');
+        break;
+
+        case  'y' :
+        alert('Yeah sure');
+        // console.log('yes');
+        break;
+        
+        case 'no' :
+            alert('wrong answer');
+            // console.log('no');
+            break;
+
+        case  'n' :
+        alert('wrong answer');
+        // console.log('no');
+        break;
+}
+
+let q4 = prompt ('DO i like games ?').toLowerCase();
+switch(q4) {
+    case 'yes' :
+        alert('Right');
+        // console.log('yes');
+        break;
+
+        case  'y' :
+        alert('Rigth');
+        // console.log('yes');
+        break;
+        
+        case 'no' :
+            alert('wrong answer');
+            // console.log('no');
+            break;
+
+        case  'n' :
+        alert('wrong answer');
+        // console.log('no');
+        break;
+}
+
+alert ('This is the end of the guessing game ' + userName + ', welcome to my web page ^_^')
+
